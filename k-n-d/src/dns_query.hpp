4f580f6202a3497a6a95e7bf19b96bbf0563e38e
k1ll3rS <877624892@qq.com>
2021-04-28 20:18:57 +0800

    [knd]* v1.0

--- k-n-d/src/dns_query.hpp
+++ k-n-d/src/dns_query.hpp
@@ -10,40 +10,40 @@
 #include "dns.h"
 
 #define MAX_DNS_PACKAGE_SIZE 1024
-#define QUERY_QEEN_SIZE 1024
+#define QUERY_QUEUE_SIZE 1024
 
-char *query_qeen[1024];
+char *query_qeen[QUERY_QUEUE_SIZE];
 
-int get_id(char *host_name){
-    for(int i=0; i<QUERY_QEEN_SIZE; i++){
+int get_dns_id(char *host_name){
+    for(int i=0; i<QUERY_QUEUE_SIZE; i++){
         if(!query_qeen[i]){
             query_qeen[i] = host_name;
             return i;
         }
     }
-    puts("[-] You are too greedy.");
+    puts("[-] You are too greedy!");
     exit(0);
 }
 
-char *get_host_name(int id){
+char *get_dns_host_name(int id){
     char *host_name = query_qeen[id];
     query_qeen[id] = NULL;
     return host_name;
 }
 
-
-uv_buf_t make_dns_query_msg(char *host_name, ssize_t len_host_name) {
+uv_buf_t make_dns_query_msg(char *host_name, ssize_t len_host_name){
     for(int i=0; i<len_host_name; i++){
         if(host_name[i] == '\n'){
             host_name[i] = '\0';
         }
     }
-	char *buf = (char *)malloc(MAX_DNS_PACKAGE_SIZE);
+    char *buf = (char *)malloc(MAX_DNS_PACKAGE_SIZE);
     memset((void *)buf, 0, MAX_DNS_PACKAGE_SIZE);
 
-    DNS_HEADER *dns_hdr = (DNS_HEADER *) buf;
-    int id = get_id(host_name);
-    //printf("[+] {%d}Query %s.\n", id, host_name);
+    DNS_HEADER *dns_hdr = (DNS_HEADER *)buf;
+    int id = get_dns_id(host_name);
+
+    printf("[+] Query for {%d}%s\n", id, host_name);
     dns_hdr->id = htons(id);
     dns_hdr->rd = 1;
     dns_hdr->qcount = htons(1);
@@ -54,7 +54,7 @@ uv_buf_t make_dns_query_msg(char *host_name, ssize_t len_host_name) {
     strcpy(buf + sizeof(DNS_HEADER)+1, host_name);
     char *p = buf+sizeof(DNS_HEADER)+1;
     u_char i = 0;
-    while (p < buf+sizeof(DNS_HEADER)+strlen(host_name)+1) {
+    while (p < buf+sizeof(DNS_HEADER)+strlen(host_name)+1){
         if(*p == '.'){
             p[-i-1] = i;
             i = 0;
@@ -74,15 +74,16 @@ uv_buf_t make_dns_query_msg(char *host_name, ssize_t len_host_name) {
             +strlen(host_name)+2);
 }
 
-void alloc_dns_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
+void alloc_dns_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf){
     *buf = uv_buf_init((char*) malloc(suggested_size), suggested_size);
+    return;
 }
 
 void on_dns_read(uv_udp_t *req, ssize_t nread, const uv_buf_t *buf,\
-        const struct sockaddr *addr, unsigned flags) {
+        const struct sockaddr *addr, unsigned flags){
     if(nread < 0){
         fprintf(stderr, "Read error %s\n", uv_err_name(nread));
-        uv_close((uv_handle_t*) req, NULL);
+        uv_close((uv_handle_t *)req, NULL);
         free(buf->base);
         return;
     }
@@ -93,7 +94,7 @@ void on_dns_read(uv_udp_t *req, ssize_t nread, const uv_buf_t *buf,\
 
         DNS_HEADER *header = (DNS_HEADER *)buf->base;
         u_int id = ntohs(header->id);
-        char *host_name = get_host_name(id);
+        char *host_name = get_dns_host_name(id);
         if (header->ancount){
             u_char *rdata = (u_char *)(buf->base+nread-4);
             printf("[*] {%u}%s -> %u.%u.%u.%u\n", id, host_name, rdata[0], rdata[1],\
@@ -105,26 +106,24 @@ void on_dns_read(uv_udp_t *req, ssize_t nread, const uv_buf_t *buf,\
         // Free the chunk allocated by alloc_stdin_bufffer
         free(host_name);
     }
+    // uv_close((uv_handle_t *)req, NULL);
     // Free the chunk allocated by alloc_dns_buffer
     free(buf->base);
+    return;
 }
 
 
-void on_dns_send(uv_udp_send_t *req, int status) {
-    if (status) {
-        fprintf(stderr, "Send error %s\n", uv_strerror(status));
+void on_dns_send(uv_udp_send_t *req, int status){
+    if (status){
+        fprintf(stderr, "[-] Send error %s\n", uv_strerror(status));
         return;
     }
 }
 
 uv_udp_t *send_socket;
-uv_udp_t *recv_socket;
 void k_dns_init(uv_loop_t *dns_loop){
     send_socket = (uv_udp_t *)malloc(sizeof(uv_udp_t));
-    recv_socket = (uv_udp_t *)malloc(sizeof(uv_udp_t));
     uv_udp_init(dns_loop, send_socket);
-    uv_udp_init(dns_loop, recv_socket);
-
     uv_udp_recv_start(send_socket, alloc_dns_buffer, on_dns_read);
     return;
 }
@@ -136,22 +135,5 @@ void k_get_addrinfo(const uv_buf_t *buf, ssize_t nread) {
     uv_buf_t dns_query_msg = make_dns_query_msg(buf->base, nread);
     uv_udp_send(send_req, send_socket, &dns_query_msg, 1,\
             (const struct sockaddr *)&send_addr, on_dns_send);
-    return;
-}
-
-void kk_get_addrinfo(const uv_buf_t *buf, ssize_t nread) {
-    uv_loop_t *dns_loop;
-    uv_udp_t *send_socket = (uv_udp_t *)malloc(sizeof(uv_udp_t));
-    dns_loop = uv_default_loop();
-    uv_udp_init(dns_loop, send_socket);
-    uv_udp_send_t send_req; 
-    struct sockaddr_in send_addr;
-    uv_ip4_addr("8.8.8.8", 53, &send_addr);
-    uv_buf_t dns_query_msg = make_dns_query_msg(buf->base, nread);
-    uv_udp_send(&send_req, send_socket, &dns_query_msg, 1,\
-            (const struct sockaddr *)&send_addr, on_dns_send);
-    send_socket->data = buf->base;
-    uv_udp_recv_start(send_socket, alloc_dns_buffer, on_dns_read);
-    uv_run(dns_loop, UV_RUN_DEFAULT);
     return;
 }
--- k-n-d/src/knDNS.c
+++ k-n-d/src/knDNS.c
@@ -5,40 +5,35 @@
 
 #include <uv.h>
 #include <uv/unix.h>
-#include "dns_query.c"
+#include "dns_query.hpp"
 
 uv_loop_t *loop;
 uv_pipe_t stdin_pipe;
 
-void alloc_stdin_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
-    *buf = uv_buf_init((char*) malloc(suggested_size), suggested_size);
+void alloc_stdin_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf){
+    *buf = uv_buf_init((char*)malloc(suggested_size), suggested_size);
+    return;
 }
 
-void read_stdin(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
-    if (nread < 0){
-        if (nread == UV_EOF){
+void read_stdin(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf){
+    if(nread < 0){
+        if(nread == UV_EOF){
             uv_close((uv_handle_t *)&stdin_pipe, NULL);
         }
-    } else if (nread > 0) {
+    } 
+    else if(nread > 0){
         k_get_addrinfo(buf, nread);		
     }
-}
-
-void test(){ uv_buf_t buf = uv_buf_init((char*) malloc(1000), 1000);
-    buf.base = "www.baidu.com";
-    for(int i=0; i<100; i++){
-        k_get_addrinfo(&buf, 10);
-        if(i%10==1){
-            uv_sleep(1000);
-        }
-    }
+    return;
 }
 
 int main(int argc, char **argv) {
     loop = uv_default_loop();
-
+    
+    // Init udp socket
     k_dns_init(loop);
 
+    // Init stdin stream
     uv_pipe_init(loop, &stdin_pipe, 0);
     uv_pipe_open(&stdin_pipe, 0);    
     uv_read_start((uv_stream_t*)&stdin_pipe, alloc_stdin_buffer, read_stdin);
